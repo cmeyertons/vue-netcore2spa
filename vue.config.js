d6e6a8cc40cb87a0cd3ad9e83c61550de6fdb52d
Carl Meyertons <carlmeyertons@gmail.com>
2018-03-27 22:39:58 -0500

    adding vue specific css

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,31 @@
-const path = require('path');
-const bundleOutputDir = './wwwroot/dist';
+const path = require("path");
+const bundleOutputDir = "./wwwroot/dist";
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   lintOnSave: false,
+  //https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
   configureWebpack: {
+    //got this from the .net core - VueJS spa template
     output: {
       path: path.join(__dirname, bundleOutputDir),
       filename: "[name].js",
       publicPath: "dist/"
-    }
+    },
+    plugins: [new ExtractTextPlugin("[name].css")]
+  },
+  chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap(options => {
+        //https://vue-loader.vuejs.org/en/configurations/extract-css.html
+        options.loaders["css"] = ExtractTextPlugin.extract({
+          use: "css-loader",
+          fallback: "vue-style-loader"
+        });
+
+        return options;
+      });
   }
 };
