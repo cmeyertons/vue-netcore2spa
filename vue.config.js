89a35697e06adcbb0bcffca26ef15215a41c5356
Carl Meyertons <carlmeyertons@gmail.com>
2018-03-28 21:33:35 -0500

    adding vuetify + getting html webpack working on the layout cshtml

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const bundleOutputDir = "./wwwroot/dist";
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const templateFolder = "./Views/Shared/";
 
 //note: you can use vue inspect > test.js to generate what the webpack.config will kinda look like, good for testing changes here
 module.exports = {
@@ -12,21 +12,24 @@ module.exports = {
       path: path.join(__dirname, bundleOutputDir),
       filename: "[name].js",
       publicPath: "dist/"
-    },
-    plugins: [new ExtractTextPlugin("[name].css")]
+    }
   },
-  chainWebpack: config => {
-    config.module
-      .rule("vue")
-      .use("vue-loader")
-      .tap(options => {
-        //https://vue-loader.vuejs.org/en/configurations/extract-css.html
-        options.loaders["css"] = ExtractTextPlugin.extract({
-          use: "css-loader",
-          fallback: "vue-style-loader"
-        });
 
-        return options;
-      });
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      var options = args[0];
+      options.template = path.join(
+        __dirname,
+        templateFolder,
+        "_LayoutTemplate.cshtml"
+      );
+
+      options.filename = path.join("../../", templateFolder, "_Layout.cshtml");
+      return args;
+    });
+
+    //https://github.com/mozilla-neutrino/webpack-chain#config-plugins-removing
+    //this was causing an issue w/ being added twice, once by .net core, once by vue-cli
+    config.plugins.delete("hmr");
   }
 };
